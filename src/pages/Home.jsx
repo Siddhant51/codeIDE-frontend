29c1c9bf6faa725d0230853f8820aa05d4173977
Siddhant51 <siddhantsonawane37@gmail.com>
2024-10-01 23:23:49 +0530

    toast, loaders and top loading bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../context/Theme";
 import { Api } from "../helper/Api";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const HomePage = ({ token, setToken }) => {
   const navigate = useNavigate();
@@ -14,7 +16,7 @@ const HomePage = ({ token, setToken }) => {
   const [projects, setProjects] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const { isLightMode, toggleTheme } = useContext(ThemeContext);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [htmlCode, setHtmlCode] = useState(`<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -77,6 +79,7 @@ const HomePage = ({ token, setToken }) => {
         );
         console.log("New project created:", response.data);
         fetchProjects();
+        toast.success("New project created successfully!");
       }
     } catch (error) {
       console.error("Error creating project:", error);
@@ -92,93 +95,100 @@ const HomePage = ({ token, setToken }) => {
   };
 
   return (
-    <div
-      className={`${
-        isLightMode ? "bg-gray-100" : "bg-gray-900"
-      } h-screen transition-all duration-300`}
-    >
-      {/* Primary Navbar */}
+    <>
+      <ToastContainer />
       <div
         className={`${
-          isLightMode ? "bg-white" : "bg-[#1A1A2E]"
-        } h-[10vh] flex items-center justify-between px-10 shadow-md transition-all duration-300`}
+          isLightMode ? "bg-gray-100" : "bg-gray-900"
+        } h-screen transition-all duration-300`}
       >
-        <h1
+        {/* Primary Navbar */}
+        <div
           className={`${
-            isLightMode ? "text-gray-800" : "text-white"
-          } text-2xl font-bold`}
+            isLightMode ? "bg-white" : "bg-[#1A1A2E]"
+          } h-[10vh] flex items-center justify-between px-10 shadow-md transition-all duration-300`}
         >
-          Code IDE
-        </h1>
-        <div className="flex items-center gap-4">
-          <button
-            onClick={handleLogout}
-            className="text-white bg-red-500 py-2 px-4 rounded-lg hover:bg-red-600 transition-all duration-300"
+          <h1
+            className={`${
+              isLightMode ? "text-gray-800" : "text-white"
+            } text-2xl font-bold`}
           >
-            Logout
-          </button>
-          <MdLightMode
-            className={`text-xl cursor-pointer transition-all duration-300 ${
-              isLightMode ? "text-gray-700" : "text-white"
-            } hover:text-green-500`}
-            onClick={toggleTheme}
+            Code IDE
+          </h1>
+          <div className="flex items-center gap-4">
+            <button
+              onClick={handleLogout}
+              className="text-white bg-red-500 py-2 px-4 rounded-lg hover:bg-red-600 transition-all duration-300"
+            >
+              Logout
+            </button>
+            <MdLightMode
+              className={`text-xl cursor-pointer transition-all duration-300 ${
+                isLightMode ? "text-gray-700" : "text-white"
+              } hover:text-green-500`}
+              onClick={toggleTheme}
+            />
+          </div>
+        </div>
+
+        {/* Secondary Navbar */}
+        <div
+          className={`${
+            isLightMode ? "bg-gray-200" : "bg-[#16213E]"
+          } h-[9vh] px-10 text-white flex items-center justify-between gap-2 transition-all duration-300`}
+        >
+          <input
+            type="text"
+            placeholder="Search projects..."
+            className="border rounded px-4 py-2 w-1/3 mr-4 text-gray-700"
+            value={searchTerm}
+            onChange={handleSearch}
           />
+          <button
+            onClick={handleCreateProject}
+            className="bg-green-500 flex items-center px-4 py-2 rounded-lg hover:bg-green-600 transition-transform"
+          >
+            <MdAdd className="mr-2" /> Create Project
+          </button>
+        </div>
+
+        {/* Projects Display Area */}
+        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {isLoading ? (
+            <>
+              <div className="animate-pulse bg-gray-300 dark:bg-gray-700 h-[10vh] shadow-md rounded-lg p-4"></div>
+              <div className="animate-pulse bg-gray-300 dark:bg-gray-700 h-[10vh] shadow-md rounded-lg p-4"></div>
+              <div className="animate-pulse bg-gray-300 dark:bg-gray-700 h-[10vh] shadow-md rounded-lg p-4"></div>
+            </>
+          ) : (
+            projects
+              .filter((project) =>
+                project.name.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .map((project) => (
+                <Link
+                  to={`/editor/${project._id}`}
+                  key={project._id}
+                  className={`${
+                    isLightMode ? "bg-white" : "bg-gray-800"
+                  } shadow-md rounded-lg p-4 text-center transition-transform transform hover:scale-105 flex items-center justify-center gap-2`}
+                >
+                  <div className="bg-sky-500 rounded-lg">
+                    <FaCode className="text-2xl text-gray-700 m-2" />{" "}
+                  </div>
+                  <h2
+                    className={`text-lg font-semibold ${
+                      isLightMode ? "text-gray-800" : "text-white"
+                    }`}
+                  >
+                    {project.name}
+                  </h2>
+                </Link>
+              ))
+          )}
         </div>
       </div>
-
-      {/* Secondary Navbar */}
-      <div
-        className={`${
-          isLightMode ? "bg-gray-200" : "bg-[#16213E]"
-        } h-[9vh] px-10 text-white flex items-center justify-between gap-2 transition-all duration-300`}
-      >
-        <input
-          type="text"
-          placeholder="Search projects..."
-          className="border rounded px-4 py-2 w-1/3 mr-4 text-gray-700"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-        <button
-          onClick={handleCreateProject}
-          className="bg-green-500 flex items-center px-4 py-2 rounded-lg hover:bg-green-600 transition-transform"
-        >
-          <MdAdd className="mr-2" /> Create Project
-        </button>
-      </div>
-
-      {/* Projects Display Area */}
-      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {isLoading ? (
-          <p>Loading projects...</p>
-        ) : (
-          projects
-            .filter((project) =>
-              project.name.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-            .map((project) => (
-              <Link
-                to={`/editor/${project._id}`}
-                key={project._id}
-                className={`${
-                  isLightMode ? "bg-white" : "bg-gray-800"
-                } shadow-md rounded-lg p-4 text-center transition-transform transform hover:scale-105 flex items-center justify-center gap-2`}
-              >
-                <div className="bg-sky-500 rounded-lg">
-                  <FaCode className="text-2xl text-gray-700 m-2" />{" "}
-                </div>
-                <h2
-                  className={`text-lg font-semibold ${
-                    isLightMode ? "text-gray-800" : "text-white"
-                  }`}
-                >
-                  {project.name}
-                </h2>
-              </Link>
-            ))
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,104 +1,120 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdLightMode } from "react-icons/md";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../context/Theme";
 import { Api } from "../helper/Api";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingBar from "react-top-loading-bar";
 
 const Login = ({ setToken }) => {
   const { isLightMode, toggleTheme } = useContext(ThemeContext);
   const [email, setEmail] = useState("test@test.com");
   const [password, setPassword] = useState("test");
   const navigate = useNavigate();
+  const loadingBar = useRef(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
+      // Show the loading bar
+      loadingBar.current.continuousStart();
       const response = await axios.post(`${Api}/login`, {
         email,
         password,
       });
-
+      // Hide the loading bar
+      loadingBar.current.complete();
       // Handle successful login
-      setToken(response.data.token);
-      localStorage.setItem("token", response.data.token);
-      navigate("/", { replace: true }); //
+      toast.success("Login successful!");
+      setTimeout(() => {
+        setToken(response.data.token);
+        localStorage.setItem("token", response.data.token);
+        navigate("/", { replace: true });
+      }, 4000);
     } catch (error) {
+      // Hide the loading bar on error
+      loadingBar.current.complete();
+      toast.error("Login failed. Please check your credentials.");
       console.error("Login failed:", error.response?.data || error.message);
-      alert("Login failed. Please check your credentials.");
     }
   };
 
   return (
-    <div
-      className={`flex flex-col items-center justify-center h-screen ${
-        isLightMode ? "bg-gray-100" : "bg-gray-900"
-      } transition-colors duration-300`}
-    >
-      <div className="absolute top-4 right-4">
-        <MdLightMode
-          onClick={toggleTheme}
-          className={`cursor-pointer text-2xl ${
-            isLightMode ? "text-gray-900" : "text-white"
-          }`}
-        />
-      </div>
+    <>
+      <ToastContainer />
+      <LoadingBar color="#f11946" ref={loadingBar} />
       <div
-        className={`w-[90%] max-w-md p-8 shadow-lg ${
-          isLightMode ? "bg-white" : "bg-gray-800"
-        } rounded-lg transition-colors duration-300`}
+        className={`flex flex-col items-center justify-center h-screen ${
+          isLightMode ? "bg-gray-100" : "bg-gray-900"
+        } transition-colors duration-300`}
       >
-        <h2
-          className={`text-2xl font-bold mb-6 ${
-            isLightMode ? "text-gray-800" : "text-white"
-          } text-center`}
+        <div className="absolute top-4 right-4">
+          <MdLightMode
+            onClick={toggleTheme}
+            className={`cursor-pointer text-2xl ${
+              isLightMode ? "text-gray-900" : "text-white"
+            }`}
+          />
+        </div>
+        <div
+          className={`w-[90%] max-w-md p-8 shadow-lg ${
+            isLightMode ? "bg-white" : "bg-gray-800"
+          } rounded-lg transition-colors duration-300`}
         >
-          Log In
-        </h2>
-        <form onSubmit={handleLogin} className="flex flex-col gap-4">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            className={`p-3 rounded-md ${
-              isLightMode
-                ? "bg-gray-200 text-gray-800"
-                : "bg-gray-700 text-white"
-            } focus:outline-none focus:ring-2 focus:ring-green-500`}
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className={`p-3 rounded-md ${
-              isLightMode
-                ? "bg-gray-200 text-gray-800"
-                : "bg-gray-700 text-white"
-            } focus:outline-none focus:ring-2 focus:ring-green-500`}
-          />
-          <button
-            type="submit"
-            className="p-3 rounded-md bg-green-500 text-white font-semibold hover:bg-green-600 transition-all duration-200"
+          <h2
+            className={`text-2xl font-bold mb-6 ${
+              isLightMode ? "text-gray-800" : "text-white"
+            } text-center`}
           >
             Log In
-          </button>
-        </form>
-        <p
-          className={`mt-4 text-center ${
-            isLightMode ? "text-gray-600" : "text-gray-400"
-          }`}
-        >
-          Don't have an account?{" "}
-          <Link to="/register" className="text-green-500 hover:underline">
-            Register
-          </Link>
-        </p>
+          </h2>
+          <form onSubmit={handleLogin} className="flex flex-col gap-4">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className={`p-3 rounded-md ${
+                isLightMode
+                  ? "bg-gray-200 text-gray-800"
+                  : "bg-gray-700 text-white"
+              } focus:outline-none focus:ring-2 focus:ring-green-500`}
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className={`p-3 rounded-md ${
+                isLightMode
+                  ? "bg-gray-200 text-gray-800"
+                  : "bg-gray-700 text-white"
+              } focus:outline-none focus:ring-2 focus:ring-green-500`}
+            />
+            <button
+              type="submit"
+              className="p-3 rounded-md bg-green-500 text-white font-semibold hover:bg-green-600 transition-all duration-200"
+            >
+              Log In
+            </button>
+          </form>
+          <p
+            className={`mt-4 text-center ${
+              isLightMode ? "text-gray-600" : "text-gray-400"
+            }`}
+          >
+            Don't have an account?{" "}
+            <Link to="/register" className="text-green-500 hover:underline">
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
